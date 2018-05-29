b1e962e44c885a0a5ffe7d8e9ce0598186382119
wdj1 <wdj1@foxmail.com>
2018-05-29 20:56:09 +0800

    updated "navbar"
    1. 修改了html和css文件中导航条部分的代码
    2. 修复了小屏幕下: 1) 导航条的展开内容不能自动折叠回去的情况  2) 点击导航条内链接后, 展开内容不能折叠的情况

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,35 @@
 // 导航条
 $(window).scroll(function () {
-  if ($(".navbar").offset().top > 50) {$(".navbar-fixed-top").addClass("top-nav");
-  }else {$(".navbar-fixed-top").removeClass("top-nav");}
-  if ($(window).width() < 751) {
-    if ($(".navbar").offset().top > 50) {$(".navbar-toggle").css({ "display": "inline" });
-    }else {$(".navbar-toggle").css({ "display": "none" });}
+  //当屏幕滚动，使导航条距离浏览器顶部大于50px时，给其加上top-nav类（加上背景颜色，padding设为0）
+  if ($(".navbar").offset().top > 50) {
+    $(".navbar-fixed-top").addClass("top-nav");
+  }else {
+    //导航条回到最顶部，删除top-nav类
+    $(".navbar-fixed-top").removeClass("top-nav");
+  }
+  //小屏幕下的导航条折叠
+  if ($(window).width() < 768) {
+    if ($(".navbar").offset().top > 50) {
+      $(".navbar-toggle").css({ "display": "inline" });
+    }else {
+      $(".navbar-toggle").css({ "display": "none" });
+    }
+
   }
 });
+//点击链接之后，把导航选项折叠起来
+if ($(window).width() < 768) {
+  $("#navbar a").click(function () {
+    $("#navbar").collapse('hide');
+  });
+  //滚动屏幕时，把导航选项折叠起来
+  $(window).scroll(function () {
+    $("#navbar").collapse('hide');
+  });
+  if ($(window).width() >= 768) {
+    $(".navbar-toggle").css({"display": "none"});
+  }
+}
 
 //初始化wow插件
 new WOW().init();
