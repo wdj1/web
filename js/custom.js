5a3eebe8f6635a9ef56ada224bb0d05352c53027
wdj1 <wdj1@foxmail.com>
2018-05-31 09:58:06 +0800

    ios下禁止缩放

--- js/custom.js
+++ js/custom.js
@@ -28,6 +28,27 @@ $(window).scroll(function () {
   }
 });
 
+if ($(window).width()<992) {
+  $(".parallax-img").css({ "background": "url(../images/parallax.jpg)"});
+}
+
+//ios禁止缩放
+window.onload=function () {
+  document.addEventListener('touchstart',function (event) {
+    if(event.touches.length>1){
+      event.preventDefault();
+    }
+  })
+  var lastTouchEnd=0;
+  document.addEventListener('touchend',function (event) {
+    var now=(new Date()).getTime();
+    if(now-lastTouchEnd<=300){
+      event.preventDefault();
+    }
+    lastTouchEnd=now;
+  },false)
+}
+
 //初始化wow插件
 new WOW().init();
 
